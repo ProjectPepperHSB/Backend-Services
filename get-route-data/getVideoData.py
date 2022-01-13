c446cc78ccf2662e2212e0b66d14423a05950d22
Jacob B. Menge <mengejacob@gmail.com>
2022-01-13 11:29:20 +0100

    add some documation
    :param names, descriptions and comments

--- get-route-data/getVideoData.py
+++ get-route-data/getVideoData.py
@@ -3,12 +3,13 @@ import requests
 
 def get_video(url: str, startpoint: str, endpoint: str, accessibily_mode: bool, format: str):
     """
-    :param url:
-    :param startpoint:
-    :param endpoint:
-    :param accessibily_mode:
-    :param format:
-    :return:
+    :param url: Download link to video (.mp4)
+    :param startpoint: start point from where the route should begin
+    :param endpoint: the end point at which the route ends (the point to which the user wants to go)
+    :param accessibily_mode: determines whether the accessibility mode should be enabled or disabled. Depending on which
+     mode is selected, a sensible path is chosen. For efficiency reasons, a string is used instead of a boolean.
+     M0000 = normal path| M0001 = Accessible path.
+    :param format:the image format in pixels. the API supports only 544x306 in most cases.
     """
 
     mode = "M0000"
@@ -37,14 +38,16 @@ if __name__ == '__main__':
 
     # Project id | Bremerhaven project id: 100011
     project = "100011"
+    # main domain
     url = f'https://cdnguide3dcom.blob.core.windows.net/videos/{project}'
+    # the image format in pixels. the API supports only 544x306 in most cases.
     video_format = '544x306'
+
     startpoint = "L00P1133"
 
     for video in range(len(videos['data']['list'])):
         endpoint = videos['data']['list'][video]['point']
         room = videos['data']['list'][video]['name']
-
         print(f'=================== ROUTE_TO_ROOM: {room} ===================')
         print("accessibily_mode: False")
         get_video(url=url,
--- get-route-data/makeRouteMetadataJSON.py
+++ get-route-data/makeRouteMetadataJSON.py
@@ -5,6 +5,7 @@ import requests
 with open('needData.json') as file:
     data = json.load(file)
 
+# temp
 new_data_struc = {}
 
 # startpoint id
@@ -19,9 +20,9 @@ def get_request(mode: str, project: str, startpoint: str, endpoint: str):
     """
     :param mode: mode for disabled access | disabled access mode True:  M0001 | disabled access mode False:  M0000
     :param project: the project used by 3Dberlin
-    :param startpoint:
-    :param endpoint:
-    :return:
+    :param startpoint: start point from where the route should begin
+    :param endpoint: the end point at which the route ends (the point to which the user wants to go)
+    :return: returns defined request as an object
     """
     url = f'https://services.guide3d.com/route/cors/index.php?project={project}&start={startpoint}&end={endpoint}&mode={mode}&redirect=duration&format=none'
     return requests.get(url, allow_redirects=True)
@@ -29,7 +30,6 @@ def get_request(mode: str, project: str, startpoint: str, endpoint: str):
 
 if __name__ == '__main__':
 
-    # print(json.dumps(data, indent=4))
     count = 0
 
     # iterate over used json data
@@ -40,7 +40,10 @@ if __name__ == '__main__':
         video_path = f'video/{video_name}'
 
         # define request for video metadata
+        # send request for normal path
         r_M0000 = get_request(mode="M0000", project=project, startpoint=startpoint, endpoint=endpoint)
+
+        # send request for Accessible path
         r_M0001 = get_request(mode="M0001", project=project, startpoint=startpoint, endpoint=endpoint)
 
         # print data
@@ -49,7 +52,7 @@ if __name__ == '__main__':
         print(f'dis: {r_M0001.json()["distance"]}')
         print(f'time: {r_M0001.json()["duration"]}')
 
-        # add new data to dictonary
+        # add new data to dictonary and define the json data structure
         new_data_struc.update(
             {f'{d["name"]}': {'M0000': {'video_path': f'{video_path}M0000.mp4',
                                         'location': f'{d["location"]}',
@@ -59,7 +62,6 @@ if __name__ == '__main__':
                                         'location': f'{d["location"]}',
                                         'distance': f'{r_M0001.json()["distance"]}',
                                         'time': f'{r_M0001.json()["duration"]}'}}})
-
         print(new_data_struc)
         print(count)
 
